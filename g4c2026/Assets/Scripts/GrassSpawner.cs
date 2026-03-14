0582b99a94e2117a8a4808baf9685f679742c162
Stwikey <115232330+Stwikey@users.noreply.github.com>
2026-03-14 17:53:44 -0400

    Fixed grass spawned & added world building objects & started working on the bus

--- g4c2026/Assets/Scripts/GrassSpawner.cs
+++ g4c2026/Assets/Scripts/GrassSpawner.cs
@@ -17,9 +17,9 @@ public class GrassSpawner : MonoBehaviour
         for(int i = 0; i < density; i++)
         {
             grassnum = Random.Range(0, grassType.Count);
-            grassx = Random.Range((float)pos.x - (float)(scale.x*10), (float)pos.x + (float)(scale.x*10));
-            grassz = Random.Range((float)pos.z - (float)(scale.z*10), (float)pos.z + (float)(scale.z*10));
-            GameObject grass = Instantiate(grassType[grassnum], new Vector3(grassx, transform.position.y, grassz), Quaternion.Euler(0, 0, 90), transform);
+            grassx = Random.Range((float)pos.x - (float)(scale.x*5), (float)pos.x + (float)(scale.x*5));
+            grassz = Random.Range((float)pos.z - (float)(scale.z*5), (float)pos.z + (float)(scale.z*5));
+            GameObject grass = Instantiate(grassType[grassnum], new Vector3(grassx, transform.position.y, grassz), Quaternion.Euler(0, Random.Range(0, 360), 90), transform);
         }
     }
 
