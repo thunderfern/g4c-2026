a6e6d43904fac6dc2765b2f90a1ce246f1f095f0
Stwikey <115232330+Stwikey@users.noreply.github.com>
2026-03-17 17:05:23 -0400

    Did some more world building

--- g4c2026/Assets/Scripts/GrassSpawner.cs
+++ g4c2026/Assets/Scripts/GrassSpawner.cs
@@ -20,6 +20,7 @@ public class GrassSpawner : MonoBehaviour
             grassx = Random.Range((float)pos.x - (float)(scale.x*5), (float)pos.x + (float)(scale.x*5));
             grassz = Random.Range((float)pos.z - (float)(scale.z*5), (float)pos.z + (float)(scale.z*5));
             GameObject grass = Instantiate(grassType[grassnum], new Vector3(grassx, transform.position.y, grassz), Quaternion.Euler(0, Random.Range(0, 360), 90), transform);
+            grass.transform.localScale = new Vector3 (1f/scale.x, 1f/scale.y, 1f/scale.z);
         }
     }
 
